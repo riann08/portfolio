0e3c74af2ebd34f6f6b7caff2827f4d56c3a9ddb
Ann Lampa <riann08@yahoo.com>
2021-03-24 08:18:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./Components/Navbar";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Navbar />
         </a>
       </header>
     </div>
